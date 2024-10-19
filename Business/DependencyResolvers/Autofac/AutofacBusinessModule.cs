4ace8cacceac6f437ccf4803b49d0ae4f68183cc
çağrı çamyar <cagri-95@hotmail.com>
2024-10-19 11:29:29 +0300

    Autofac DI yapildi ve Validation Eklendi

---
+++ Business/DependencyResolvers/Autofac/AutofacBusinessModule.cs
@@ -0,0 +1,28 @@
+﻿using Autofac;
+using Autofac.Extras.DynamicProxy;
+using Business.Abstract;
+using Business.Concrete;
+using Castle.DynamicProxy;
+using DataAccess.Abstract;
+using DataAccess.Concrete.EntityFramework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.DependencyResolvers.Autofac
+{
+    public class AutofacBusinessModule : Module
+    {
+
+        protected override void Load(ContainerBuilder builder)
+        {
+            builder.RegisterType<BookManager>().As<IBookService>().SingleInstance();
+            builder.RegisterType<EfBookDal>().As<IBookDal>().SingleInstance();
+
+            builder.RegisterType<AuthorManager>().As<IAuthorService>().SingleInstance();
+            builder.RegisterType<EfAuthorDal>().As<IAuthorDal>().SingleInstance();
+        }
+    }
+}
--- WebAPI/Program.cs
+++ WebAPI/Program.cs
@@ -1,6 +1,8 @@
-
+using Autofac.Extensions.DependencyInjection;
+using Autofac;
 using Business.Abstract;
 using Business.Concrete;
+using Business.DependencyResolvers.Autofac;
 using DataAccess.Abstract;
 using DataAccess.Concrete.EntityFramework;
 
@@ -15,15 +17,18 @@ public class Program
         // Add services to the container.
 
         builder.Services.AddControllers();
-        builder.Services.AddSingleton<IAuthorService, AuthorManager>();
-        builder.Services.AddSingleton<IAuthorDal, EfAuthorDal>();
-        builder.Services.AddSingleton<IBookService, BookManager>();
-        builder.Services.AddSingleton<IBookDal, EfBookDal>();
+        //builder.Services.AddSingleton<IAuthorService, AuthorManager>();
+        //builder.Services.AddSingleton<IAuthorDal, EfAuthorDal>();
+        //builder.Services.AddSingleton<IBookService, BookManager>();
+        //builder.Services.AddSingleton<IBookDal, EfBookDal>();
         builder.Services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
         // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
         builder.Services.AddEndpointsApiExplorer();
         builder.Services.AddSwaggerGen();
 
+        builder.Host.UseServiceProviderFactory(services => new AutofacServiceProviderFactory())
+            .ConfigureContainer<ContainerBuilder>(builder => { builder.RegisterModule(new AutofacBusinessModule()); });
+       
         var app = builder.Build();
 
         // Configure the HTTP request pipeline.
