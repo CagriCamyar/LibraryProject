18e3235bf2e1a16eb51b7c797a4e366fced9d112
çağrı çamyar <cagri-95@hotmail.com>
2024-10-21 23:41:02 +0300

    User ve Yetkilendirmeleri Eklendi

--- Core/Entities/Concrete/User.cs
+++ Core/Entities/Concrete/User.cs
@@ -7,6 +7,13 @@ using System.Threading.Tasks;
 namespace Core.Entities.Concrete
 {
     public class User
-    {
+    {       
+        public int UserId { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public byte[] PasswordHash { get; set; }
+        public byte[] PasswordSalt { get; set; }
+        public bool Status { get; set; }
     }
 }
